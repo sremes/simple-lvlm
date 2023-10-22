0bc615a17efecfeef0b057beb707e55dac1ad886
Sami Remes <sami.remes@gmail.com>
2023-10-22 16:05:02 +0300

    improve train.py

--- train.py
+++ train.py
@@ -16,6 +16,10 @@ logging.basicConfig(filename="train.log", encoding="utf-8", level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler())
 
 
+class AnnotationException(Exception):
+    pass
+
+
 class LLaVADataset(Dataset):
     def __init__(
         self,
@@ -29,73 +33,92 @@ class LLaVADataset(Dataset):
         self.image_dir = image_dir
         self.image_processor = image_processor
 
+    @staticmethod
+    def sample_qa_pair(conversations: list[dict[str, str]]) -> tuple[str, str]:
+        qa_pairs = list(pairwise(conversations))[::2]
+        qa_pair = random.sample(qa_pairs, 1)[0]
+        question = qa_pair[0]["value"].replace("<image>", "").replace("\n", "")
+        answer = qa_pair[1]["value"].replace("\n", " ")
+        return question, answer
+
     def __getitem__(self, index: int) -> dict[str, torch.Tensor | str]:
         while True:
-            annotation = self.annotations[index]
-            if "image" not in annotation.keys():
-                logging.debug(f"annotation has no image: {annotation}")
+            try:
+                annotation = self.annotations[index]
+                if "image" not in annotation.keys():
+                    raise AnnotationException(f"annotation has no image: {annotation}")
+                image_path = Path(self.image_dir, annotation["image"])
+                if not image_path.exists():
+                    raise AnnotationException(f"image not found: {image_path}")
+            except AnnotationException as e:
+                logging.debug(e)
                 index = random.sample(range(len(self)), 1)[0]
                 continue
-            image_path = Path(self.image_dir, annotation["image"])
-            if not image_path.exists():
-                logging.debug(f"image not found: {image_path}")
-                index = random.sample(range(len(self)), 1)[0]
-                continue
-            break
+            else:
+                break
         image = PIL.Image.open(image_path)
         image = self.image_processor(images=image, return_tensors="pt")["pixel_values"]
-        conversation = random.sample(
-            list(pairwise(annotation["conversations"]))[::2], 1
-        )[0]
-        human = conversation[0]["value"].replace("<image>", "").replace("\n", "")
-        gpt = conversation[1]["value"].replace("\n", "")
+        question, answer = self.sample_qa_pair(annotation["conversations"])
         return {
-            "image": image.squeeze(),
-            "text_input": human,
-            "text_output": gpt,
+            "image": image.squeeze().to(torch.bfloat16),
+            "text_input": question,
+            "text_output": answer,
         }
 
     def __len__(self) -> int:
         return len(self.annotations)
 
 
-def save_checkpoint(model: torch.nn.Module, optimizer: torch.optim.Optimizer, epoch: int, path: Path) -> None:
-    param_has_grad = {
-        k: v.requires_grad for (k, v) in model.named_parameters()
-    }
+def save_checkpoint(
+    model: torch.nn.Module, optimizer: torch.optim.Optimizer, epoch: int, path: Path
+) -> None:
+    param_has_grad = {k: v.requires_grad for (k, v) in model.named_parameters()}
     state_dict = model.state_dict()
     for k in list(state_dict.keys()):
         if not param_has_grad.get(k, False):
             del state_dict[k]
-    torch.save({
-        'epoch': epoch,
-        'model_state_dict': state_dict,
-        'optimizer_state_dict': optimizer.state_dict(),
-    }, path)
+    torch.save(
+        {
+            "epoch": epoch,
+            "model_state_dict": state_dict,
+            "optimizer_state_dict": optimizer.state_dict(),
+        },
+        path,
+    )
+
+
+def load_checkpoint(
+    model: torch.nn.Module, optimizer: torch.optim.Optimizer, path: Path
+) -> None:
+    ckpt = torch.load(path, map_location="cpu")
+    model.load_state_dict(ckpt["model_state_dict"], strict=False)
+    optimizer.load_state_dict(ckpt["optimizer_state_dict"])
 
 
 def train(model: torch.nn.Module, data_loader: DataLoader, num_epochs: int = 1) -> None:
     optimizer = torch.optim.AdamW(
         filter(lambda x: x.requires_grad, model.parameters()), lr=1e-4
     )
+    ckpt_path = Path("checkpoints/last.ckpt")
+    if ckpt_path.exists():
+        load_checkpoint(model, optimizer, ckpt_path)
     for epoch in range(num_epochs):
         logging.info(f"Epoch: {epoch}")
-        for it, data in enumerate(bar := tqdm.tqdm(data_loader)):
+        for step, data in enumerate(bar := tqdm.tqdm(data_loader)):
             optimizer.zero_grad()
-            with torch.autocast("cuda"):
-                loss = model(
-                    image=data["image"].to(model.device),
-                    text_input=data["text_input"],
-                    text_output=data["text_output"],
-                )
+            loss = model(
+                image=data["image"].to(model.device),
+                text_input=data["text_input"],
+                text_output=data["text_output"],
+            )
             loss.backward()
             optimizer.step()
 
             bar.set_description(f"Training progress")
-            bar.set_postfix({"iter": it, "loss": loss.cpu().item()})
-            if it == 100:
-                break
-        save_checkpoint(model, optimizer, epoch, Path("checkpoints/last.ckpt"))
+            bar.set_postfix({"iter": step, "loss": loss.cpu().item()})
+            if step % 1000 == 0:
+                logging.info(f"saving checkpoint at epoch {epoch}, iter {step}")
+                save_checkpoint(model, optimizer, epoch, ckpt_path)
 
 
 if __name__ == "__main__":
@@ -108,5 +131,5 @@ if __name__ == "__main__":
         image_dir=Path("/stash/datasets"),
         image_processor=model.image_processor,
     )
-    data_loader = DataLoader(dataset, batch_size=2, shuffle=True, num_workers=2)
+    data_loader = DataLoader(dataset, batch_size=8, shuffle=True, num_workers=2)
     train(model, data_loader, num_epochs=1)
