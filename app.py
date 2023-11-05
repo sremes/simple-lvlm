8cdde1715fe4ad3b8b3a04bdb2f0ec2755bd6bc1
Sami Remes <sami.remes@gmail.com>
2023-11-05 21:14:36 +0200

    add more options for generation

--- app.py
+++ app.py
@@ -23,7 +23,16 @@ def load_image(image_path: Path) -> torch.Tensor:
     return model.image_processor(image, return_tensors="pt")["pixel_values"]
 
 
-def infer(image_path: Path, prompt: str) -> str:
+def infer(
+    image_path: Path,
+    prompt: str,
+    max_tokens: int = 128,
+    temperature: float = 0.75,
+    top_p: float = 0.95,
+    num_beams: int = 4,
+    length_penalty: float = -1.0,
+    repetition_penalty: float = 1.2,
+) -> str:
     global model
     if not model:
         load_model("checkpoints/last.ckpt")
@@ -31,17 +40,41 @@ def infer(image_path: Path, prompt: str) -> str:
     return model.generate(
         image,
         prompt,
-        max_new_tokens=64,
+        max_new_tokens=max_tokens,
         do_sample=True,
-        num_beams=4,
+        temperature=temperature,
+        top_p=top_p,
+        num_beams=num_beams,
+        length_penalty=length_penalty,
+        repetition_penalty=repetition_penalty,
     )
 
 
 with gr.Blocks() as demo:
     image = gr.Image(label="Image", type="filepath")
-    prompt = gr.Textbox(label="Prompt", placeholder="What is in the image?")
+    prompt = gr.Textbox(label="Question", placeholder="What is in the image?")
+    max_tokens = gr.Slider(1, 256, 128, step=1, label="Max tokens")
+    temperature = gr.Slider(0.0, 1.0, 0.75, step=0.05, label="Temperature")
+    top_p = gr.Slider(0.0, 1.0, 0.95, step=0.05, label="Top-P")
+    num_beams = gr.Slider(1, 8, 4, step=1, label="Number of beams")
+    length_penalty = gr.Slider(-5.0, 5.0, -1.0, step=0.1, label="Length penalty")
+    repetition_penalty = gr.Slider(-5.0, 5.0, 1.2, step=0.1, label="Repetition penalty")
     button = gr.Button("Get answer!")
     output = gr.Textbox(label="Output")
-    button.click(fn=infer, inputs=[image, prompt], outputs=output, api_name="image-qa")
+    button.click(
+        fn=infer,
+        inputs=[
+            image,
+            prompt,
+            max_tokens,
+            temperature,
+            top_p,
+            num_beams,
+            length_penalty,
+            repetition_penalty,
+        ],
+        outputs=output,
+        api_name="image-qa",
+    )
 
 demo.launch(server_name="0.0.0.0")
