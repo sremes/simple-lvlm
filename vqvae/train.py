ae39019afc7f32e078d31a64f19c2b5c8e078c5c
Sami Remes <sami.remes@gmail.com>
2025-03-16 18:40:19 +0000

    stabilize vqvae training by better init, cosine similarity, synchronized update rule, etc.

--- vqvae/train.py
+++ vqvae/train.py
@@ -6,12 +6,23 @@ import torch
 import torchvision
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import v2
-from torchvision.io import read_image, ImageReadMode
+from torchvision.io import decode_image, ImageReadMode
 from torch.utils.tensorboard import SummaryWriter
 
 import argparse
 from functools import partial
 import os
+from itertools import islice
+
+# torch._inductor.config.max_autotune = True
+# torch._inductor.max_autotune_gemm_backends = "TRITON"
+# torch._inductor.max_autotune_conv_backends = "TRITON"
+# torch._inductor.config.coordinate_descent_tuning = True
+# # torch._inductor.config.freezing = True
+# torch._inductor.config.layout_optimization = True
+# torch._inductor.config.cpp_wrapper = True
+# torch._inductor.config.rocm.compile_opt_level = "-Ofast"
+# os.environ["PYTORCH_MIOPEN_SUGGEST_NHWC"] = "1"
 
 
 def create_dataloader(dataset_path: str, batch_size: int, dtype: torch.dtype):
@@ -26,7 +37,9 @@ def create_dataloader(dataset_path: str, batch_size: int, dtype: torch.dtype):
     dataset = ImageFolder(
         dataset_path,
         transform=transform,
-        loader=partial(read_image, mode=ImageReadMode.RGB, apply_exif_orientation=True),
+        loader=partial(
+            decode_image, mode=ImageReadMode.RGB, apply_exif_orientation=True
+        ),
     )
     dataloader = torch.utils.data.DataLoader(
         dataset, batch_size=batch_size, shuffle=True, num_workers=16, pin_memory=True
@@ -34,36 +47,104 @@ def create_dataloader(dataset_path: str, batch_size: int, dtype: torch.dtype):
     return dataloader
 
 
+def log_model_summary(model: torch.nn.Module, writer: SummaryWriter):
+    print(model)
+    writer.add_text("model_summary", str(model))
+    writer.add_text("model_params", str(sum(p.numel() for p in model.parameters())))
+    writer.add_text(
+        "model_trainable_params",
+        str(sum(p.numel() for p in model.parameters() if p.requires_grad)),
+    )
+
+
 def train(args):
+    # create dataloader and model
     dataloader = create_dataloader(args.dataset_path, args.batch_size, args.dtype)
+    # num_classes = len(dataloader.dataset.classes)
+    # model = VQVAE(num_classes=num_classes).to(device="cuda", dtype=args.dtype)
     model = VQVAE().to(device="cuda", dtype=args.dtype)
+    # create optimizer and scheduler
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.AdamW(params, lr=args.lr, fused=True)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+        optimizer, T_0=50, T_mult=2
+    )
+    # load state or init
     if args.ckpt is not None:
-        model.load_state_dict(torch.load(args.ckpt, map_location="cpu"), strict=False)
+        checkpoint = torch.load(args.ckpt, map_location="cpu")
+        model.load_state_dict(checkpoint["model"])
+        optimizer.load_state_dict(checkpoint["optimizer"])
+        del checkpoint
+        global_step = checkpoint["step"]
     else:
         model.apply(init_weights)
-    params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.AdamW(params, lr=args.lr)
+        global_step = 0
+    # setup tensorboard
     writer = SummaryWriter()
-    for epoch in range(1):
-        for iter, batch in enumerate(dataloader):
+    writer.add_hparams(
+        {
+            "batch_size": args.batch_size,
+            "lr": args.lr,
+            "dtype": str(args.dtype),
+            "dataset_path": args.dataset_path,
+        },
+        {},
+    )
+    log_model_summary(model, writer)
+    # start training
+    last_epoch = global_step // len(dataloader)
+    last_step = global_step % len(dataloader)
+    for epoch in range(last_epoch, 1):
+        # first skip last_step steps from dataloader
+        if epoch == last_epoch and last_step > 0:
+            data_iterator = enumerate(
+                islice(dataloader, last_step, None), start=last_step
+            )
+        else:
+            data_iterator = enumerate(dataloader)
+        for iter, batch in data_iterator:
+            global_step = epoch * len(dataloader) + iter
             optimizer.zero_grad()
+            # img, labels = batch
             img, _ = batch
             img = img.to(device="cuda", dtype=args.dtype)
-            img_recon, loss = model(img)
+            # labels = labels.to(device="cuda", dtype=torch.long)
+            with torch.autocast(
+                device_type="cuda", enabled=(args.dtype == torch.float32)
+            ):
+                # img_recon, loss = model(img, labels)
+                img_recon, loss = model(img)
             loss.backward()
             optimizer.step()
+            scheduler.step()
             print(f"epoch: {epoch} iter: {iter} loss: {loss.item()}")
-            writer.add_scalar("loss", loss.item(), epoch * len(dataloader) + iter)
+            writer.add_scalar("loss", loss.item(), global_step)
+            writer.add_scalar("lr", optimizer.param_groups[0]["lr"], global_step)
             if iter % 50 == 0:
-                grid = torchvision.utils.make_grid([img[0], img_recon[0]], nrow=1)
-                writer.add_image("img_recon", grid, epoch * len(dataloader) + iter)
+                grid = torchvision.utils.make_grid(
+                    [img[0], img_recon[0]], nrow=1, normalize=True, scale_each=True
+                )
+                writer.add_image("img_recon", grid, global_step)
             if iter % 500 == 0:
                 if not os.path.exists("checkpoints"):
                     os.makedirs("checkpoints")
                 torch.save(
-                    model.state_dict(),
-                    f"checkpoints/vqvae_epoch{epoch}-step{iter}-loss{loss.item():.3f}.pth",
+                    {
+                        "model": model.state_dict(),
+                        "optimizer": optimizer.state_dict(),
+                        "step": global_step,
+                    },
+                    f"checkpoints/vqvae_epoch{epoch:02d}-step{iter:06d}-loss{loss.item():.3f}.pth",
                 )
+        # save after full epoch
+        torch.save(
+            {
+                "model": model.state_dict(),
+                "optimizer": optimizer.state_dict(),
+                "step": global_step,
+            },
+            f"checkpoints/vqvae_epoch{epoch}-loss{loss.item():.3f}.pth",
+        )
     writer.close()
 
 
--- vqvae/vqvae.py
+++ vqvae/vqvae.py
@@ -2,10 +2,18 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from einops import rearrange
+
 from flash_attn import flash_attn_func
-import lpips
+
+from argb.argb import ArgbPixelEncoder
 
 from functools import partial
+from typing import Optional
+
+from einops._torch_specific import allow_ops_in_compiled_graph  # requires einops>=0.6.1
+
+allow_ops_in_compiled_graph()
+torch.set_float32_matmul_precision("medium")
 
 
 class PatchEmbed(nn.Module):
@@ -42,6 +50,7 @@ class Attention(nn.Module):
         self.proj = nn.Linear(embed_dim, embed_dim)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        ## FLASHATTN
         q, k, v = rearrange(
             self.qkv(x),
             "b n (qkv h c) -> qkv b n h c",
@@ -49,16 +58,9 @@ class Attention(nn.Module):
             h=self.num_heads,
             c=self.head_dim,
         ).unbind(0)
-        # x = flash_attn_func(q, k, v, causal=False, softmax_scale=self.scale)
-        x = F.scaled_dot_product_attention(
-            q.transpose(1, 2),
-            k.transpose(1, 2),
-            v.transpose(1, 2),
-            dropout_p=0.0,
-            is_causal=False,
-            scale=self.scale,
-        ).transpose(1, 2)
+        x = flash_attn_func(q, k, v, causal=False, softmax_scale=self.scale)
         x = rearrange(x, "b n h d -> b n (h d)")
+
         x = self.proj(x)
         return x
 
@@ -87,8 +89,10 @@ class Encoder(nn.Module):
         in_channels: int = 3,
         img_size: int = 224,
         embed_dim: int = 768,
-        num_heads: int = 8,
+        num_heads: int = 24,
         patch_size: int = 8,
+        num_layers: int = 8,
+        quant_dim: int = 32,
     ):
         super().__init__()
         self.in_channels = in_channels
@@ -100,17 +104,24 @@ class Encoder(nn.Module):
             embed_dim=embed_dim,
             patch_size=patch_size,
         )
+        self.position_embeddings = nn.Parameter(
+            0.02 * torch.randn(1, self.patch_embed.num_patches, embed_dim)
+        )
         self.encoder = nn.Sequential(
             *[
                 TransformerBlock(embed_dim=embed_dim, num_heads=num_heads)
-                for _ in range(2)
+                for _ in range(num_layers)
             ]
+        )
+        self.proj = nn.Sequential(
+            nn.Linear(embed_dim, quant_dim), nn.LayerNorm(quant_dim, eps=1e-4)
         )
 
     def forward(self, image: torch.Tensor) -> torch.Tensor:
         x = self.patch_embed(image)
+        x = x + self.position_embeddings
         x = self.encoder(x)
-        return x
+        return self.proj(x)
 
 
 class Decoder(nn.Module):
@@ -119,36 +130,55 @@ class Decoder(nn.Module):
         out_channels: int = 3,
         img_size: int = 224,
         embed_dim: int = 768,
-        num_heads: int = 8,
+        num_heads: int = 24,
         patch_size: int = 8,
+        num_layers: int = 8,
+        quant_dim: int = 32,
     ):
         super().__init__()
         self.out_channels = out_channels
         self.embed_dim = embed_dim
         self.img_size = img_size
         self.patch_size = patch_size
+        self.proj = nn.Linear(quant_dim, embed_dim)
         self.decoder = nn.Sequential(
             *[
                 TransformerBlock(embed_dim=embed_dim, num_heads=num_heads)
-                for _ in range(2)
+                for _ in range(num_layers)
             ]
         )
-        self.upsample = nn.ConvTranspose2d(
-            embed_dim,
-            out_channels,
-            kernel_size=patch_size,
-            stride=patch_size,
+        num_patches = (img_size // patch_size) ** 2
+        self.position_embeddings = nn.Parameter(
+            0.02 * torch.randn(1, num_patches, embed_dim)
         )
+        self.to_patch_pixels = nn.Linear(
+            embed_dim, patch_size * patch_size * out_channels
+        )
+        # self.refine = nn.Sequential(
+        #     nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+        #     nn.BatchNorm2d(out_channels),
+        #     nn.ReLU(),
+        #     nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+        #     nn.BatchNorm2d(out_channels),
+        #     nn.ReLU(),
+        #     nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+        # )
+        self.dropout = nn.Dropout(0.1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.proj(x) + self.position_embeddings
         x = self.decoder(x)
+        x = self.dropout(x)
         x = rearrange(
-            x,
-            "b (h w) c -> b c h w",
+            self.to_patch_pixels(x),
+            "b (h w) (p1 p2 c) -> b c (h p1) (w p2)",
             h=self.img_size // self.patch_size,
             w=self.img_size // self.patch_size,
+            p1=self.patch_size,
+            p2=self.patch_size,
+            c=self.out_channels,
         )
-        x = self.upsample(x)
+        # x = self.refine(x)
         return x
 
 
@@ -159,51 +189,94 @@ class VQVAE(nn.Module):
         in_channels: int = 3,
         img_size: int = 224,
         embed_dim: int = 768,
-        num_heads: int = 12,  # head dim = embed_dim (768) // num_heads  (12)= 64
+        quant_dim: int = 64,
+        num_heads: int = 12,  # head dim = embed_dim (768) // num_heads (12) = 64
         patch_size: int = 8,
-        beta: float = 0.25,
+        alpha: float = 10.0,
+        beta: float = 0.1,
+        sync_nu: float = 0.1,
+        num_classes: Optional[int] = None,
     ):
         super().__init__()
-        self.encoder = Encoder(in_channels, img_size, embed_dim, num_heads, patch_size)
-        self.decoder = Decoder(in_channels, img_size, embed_dim, num_heads, patch_size)
-        self.perceptual_loss = lpips.LPIPS(net="alex", verbose=False)
+        self.encoder = Encoder(
+            in_channels, img_size, embed_dim, num_heads, patch_size, quant_dim=quant_dim
+        )
+        self.decoder = Decoder(
+            in_channels, img_size, embed_dim, num_heads, patch_size, quant_dim=quant_dim
+        )
+        # load pretrained model for reconstruct loss with frozen weights
+        # self.recon_loss = ArgbPixelEncoder.from_pretrained("argb/argb-ecoset-224px.pth")
+        # self.recon_loss.eval()
+        # for param in self.recon_loss.parameters():
+        # param.requires_grad = False
+        # assert self.recon_loss.training == False
+        self.alpha = alpha
         self.beta = beta
-        self.embeddings = nn.Embedding(num_embeddings, embed_dim)
-        self.embeddings.weight.data.normal_(std=0.02)
+        self.embeddings = nn.Embedding(num_embeddings, quant_dim)
+        self.embedding_mean = nn.Parameter(
+            torch.zeros_like(self.embeddings.weight.mean(0, keepdim=True))
+        )
+        self.embedding_std = nn.Parameter(
+            torch.log(torch.ones_like(self.embeddings.weight.mean(0, keepdim=True)))
+        )
+        self.sync_nu = sync_nu
+        if num_classes is not None:
+            self.classifier = nn.Linear(quant_dim, num_classes)
 
-    def forward(self, image: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self, image: torch.Tensor, label: Optional[torch.Tensor] = None
+    ) -> torch.Tensor:
         """Follow notation of https://arxiv.org/pdf/1711.00937"""
-        # encode and decode
+        # encode
         z_e = self.encoder(image)
+        # quantize
         z_q = self.find_closest_embedding(z_e)
         # calculate vq loss
-        loss_vq = ((z_e.detach() - z_q) ** 2).mean() + self.beta * (
+        loss_vq = ((1 - self.beta) * (z_e.detach() - z_q) ** 2).mean() + self.beta * (
             (z_e - z_q.detach()) ** 2
         ).mean()
-        # preserve gradients
-        # z_q = z_e + (z_q - z_e).detach()
+        # preserve gradients (straight-through gradient estimator) with the synchronized update rule
+        z_q = z_e + (z_q - z_e).detach() + self.sync_nu * (z_q - z_q.detach())
         # decode from the latent
+        z_q = F.dropout(z_q, p=0.1, training=self.training)
         x = self.decoder(z_q)
 
-        # recon losses
-        loss_mse = ((image - x) ** 2).mean()
-        loss_abs = (image - x).abs().mean()
-        loss_perceptual = self.perceptual_loss(
-            x.clamp(-1, 1), image.clamp(-1, 1)
-        ).mean()
+        # calculate reconstruction loss
+        loss_recon = (
+            # 10 * self.recon_loss.loss(x, image)
+            F.mse_loss(x, image)
+            + F.l1_loss(x, image)
+        )
 
-        # total loss
-        loss = loss_vq + loss_mse + loss_abs + loss_perceptual
+        # optionally compute a classification loss
+        if label is not None:
+            y = self.classifier(z_q.mean(1))
+            loss_class = F.cross_entropy(y, label)
+            return x, self.alpha * loss_vq + loss_recon + loss_class
 
+        # total loss without classification loss
+        loss = self.alpha * loss_vq + loss_recon
         return x, loss
 
     def find_closest_embedding(self, z_e: torch.Tensor) -> torch.Tensor:
         """Find the closest embeddings to z_e."""
         z_e_shape = z_e.shape
         z_e = rearrange(z_e, "b n c -> (b n) c")
-        distances = torch.cdist(z_e, self.embeddings.weight)
-        indices = torch.argmin(distances, dim=1)
-        embeddings = self.embeddings(indices).view(z_e_shape)
+        # affine parameterization of the codebook embeddings
+        embedding_std = self.embedding_std.exp()
+        with torch.no_grad():
+            all_embeddings = (
+                embedding_std * self.embeddings.weight + self.embedding_mean
+            )
+            all_embeddings = F.normalize(all_embeddings, dim=1)
+            distances = torch.cdist(F.normalize(z_e, dim=1), all_embeddings)
+            if self.training:
+                distances = distances * (0.95 + 0.1 * torch.rand_like(distances))
+            indices = torch.argmin(distances, dim=1)
+        # return the closest embeddings transformed by the affine parameters
+        embeddings = (
+            embedding_std * self.embeddings(indices) + self.embedding_mean
+        ).view(z_e_shape)
         return embeddings
 
 
@@ -213,28 +286,19 @@ def init_weights(
 ) -> None:
     """Initialize the weights"""
     if isinstance(module, (nn.Linear, nn.Conv2d)):
-        # Upcast the input in `fp32` and cast it back to desired `dtype` to avoid
-        # `trunc_normal_cpu` not implemented in `half` issues
-        module.weight.data = nn.init.trunc_normal_(
-            module.weight.data.to(torch.float32),
-            mean=0.0,
-            std=initializer_range,
-        ).to(module.weight.dtype)
-        if module.bias is not None:
-            module.bias.data.zero_()
+        module.reset_parameters()
     elif isinstance(module, nn.LayerNorm):
         module.bias.data.zero_()
         module.weight.data.fill_(1.0)
     elif isinstance(module, nn.Embedding):
-        module.weight.data = nn.init.trunc_normal_(
-            module.weight.data.to(torch.float32),
-            mean=0.0,
-            std=initializer_range,
-        ).to(module.weight.dtype)
+        num_embeddings, _ = module.weight.size()
+        nn.init.uniform_(module.weight, -1 / num_embeddings, 1 / num_embeddings)
+        with torch.no_grad():
+            module.weight /= module.weight.norm(dim=1, keepdim=True)
 
 
 if __name__ == "__main__":
-    dtype = torch.bfloat16
+    dtype = torch.float16
     grad_scale = 1
     model = VQVAE(num_embeddings=10).to(device="cuda", dtype=dtype)
     model.apply(partial(init_weights, initializer_range=0.02))
